8030e5566796c0dd7860976bb7ccfbf2b24ea868
Arjun Singh <arjunsinghrajawata1@gmail.com>
2024-08-05 00:35:49 +0530

    fixed fetch Projects url

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -4,20 +4,22 @@ import Prefooter from "@/components/ui/Prefooter/Prefooter";
 import { HeroProjectsParallax } from "./ui/HeroProjectsParallax";
 import ProjectsSection from "./ui/ProjectsSection";
 
-import { store } from "@/redux-store/store";
-import { fetchProjects } from "@/redux-store/slices/projectsSlice";
+// import { instance } from "@/lib/axios";
+// import { store } from "@/redux-store/store";
+// import { fetchProjects } from "@/redux-store/slices/projectsSlice";
 
 async function ProjectsPage() {
-  const { payload = null } = await store.dispatch(fetchProjects());
+  const response:any = await fetch(`${process.env.APP_URL}/api/projects`); // store.dispatch(fetchProjects());
+  const data:any = await response.json();
 
   return (
     <div>
-      {!!payload && <div>Loading...</div>}
-      
-      {payload?.success ? (
+      {!data && <div>Loading...</div>}
+
+      {data?.success ? (
         <>
-          <HeroProjectsParallax products={payload.projects} />
-          <ProjectsSection projects={payload.projects} />
+          <HeroProjectsParallax products={data.projects} />
+          <ProjectsSection projects={data.projects} />
         </>
       ) : (
         <div>Error in Fetching Projects</div>
